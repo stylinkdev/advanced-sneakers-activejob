eafaaaf320c51c47253c4b084477fca5bec54d22
Thanos Bellos <contact@thanosbellos.com>
2023-10-08 15:30:03 +0300

    Feat: Update for rails 7.1

--- lib/advanced_sneakers_activejob/workers_registry.rb
+++ lib/advanced_sneakers_activejob/workers_registry.rb
@@ -50,13 +50,16 @@ module AdvancedSneakersActiveJob
 
     def activejob_workers
       define_active_job_consumers
+      if ActionMailer.gem_version >= Gem::Version.new("7.1.0")
+        define_action_mailer_consumers
+      end
 
       @activejob_workers
     end
 
-    def method_missing(method_name, *args, &block)
+    def method_missing(method_name, *, &block)
       if call.respond_to?(method_name)
-        call.send(method_name, *args, &block)
+        call.send(method_name, *, &block)
       else
         super
       end
@@ -68,7 +71,25 @@ module AdvancedSneakersActiveJob
 
     def define_active_job_consumers
       active_job_classes_with_matching_adapter.each do |worker|
+        # after upgrading to rails 7.1.0, #worker.new.queue_name for ActionMailer::MailDeliveryJob returns is calculated dynamically based on the job's arguments.
+        # it's now each mailer class that can define its own queue_name and even a custom delivery_job
+        # if a custom delivery_job is defined, it is its responsibility to define its own queue_name in away that will work with advanced_sneakers_adapter
+        next if ActionMailer.gem_version >= Gem::Version.new("7.1.0") && worker == ActionMailer::MailDeliveryJob
+
         AdvancedSneakersActiveJob.define_consumer(queue_name: worker.new.queue_name)
+      end
+    end
+
+    def define_action_mailer_consumers
+      mailer_classes = action_mailer_classes_with_matching_adapter
+      mailer_classes.each do |mailer|
+        # Before the changes in rails 7.1.0, when the ActionMailer#deliver_later_queue_name was nil
+        # The queue_name was set to set to default ActiveJob queue_name ("default")
+        # https://github.com/rails/rails/blob/961fc42f90e880516e8ec28cacc88e2425c78110/activejob/lib/active_job/queue_name.rb#L63
+        # Since Rails 6.1, ActionMailer#deliver_later_queue_name is configured to nil
+        # so we need to manually set a default value if the developers don't configured to a desired value
+        queue_name = mailer.deliver_later_queue_name || "mailers" # "default"
+        AdvancedSneakersActiveJob.define_consumer(queue_name:)
       end
     end
 
@@ -78,6 +99,14 @@ module AdvancedSneakersActiveJob
       ([ActiveJob::Base] + ActiveJob::Base.descendants).select do |klass|
         klass.queue_adapter == ::ActiveJob::QueueAdapters::AdvancedSneakersAdapter ||
           klass.queue_adapter.is_a?(::ActiveJob::QueueAdapters::AdvancedSneakersAdapter)
+      end
+    end
+
+    def action_mailer_classes_with_matching_adapter
+      active_job_classes = active_job_classes_with_matching_adapter
+      mailer_classes = ActionMailer::Base.descendants
+      mailer_classes.select do |klass|
+        active_job_classes.include?(klass.delivery_job)
       end
     end
   end
